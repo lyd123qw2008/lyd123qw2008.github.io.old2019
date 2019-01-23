5459a0f0be2f7be1fef3c6a21f4b51a83a6f94e5
lyd123qw2008 <326643467@qq.com>
2019-01-23 22:50:15 +0800

    Site updated: 2019-01-23 22:50:15

--- js/posts.js
+++ js/posts.js
@@ -1,1 +1,1 @@
-[{"title":"年鉴2018","date":"2019.01.22","uri":"2019/01/22/年鉴/","excerpt":""},{"title":"Hello World","date":"2019.01.22","uri":"2019/01/22/hello-world/","excerpt":""}]
+[{"title":"年鉴2018","date":"2019.01.22","uri":"2019/01/22/年鉴/","excerpt":""},{"title":"Hello World","date":"2019.01.22","uri":"2019/01/22/hello-world/","excerpt":""},{"title":"与时间赛跑","date":"2019.01.23","uri":"2019/01/23/与时间赛跑/","excerpt":""}]
